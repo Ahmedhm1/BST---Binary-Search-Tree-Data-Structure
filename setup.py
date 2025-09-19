92f38521b72af5bcd27049d80729faa44d302d7d
Ahmed Hisham <139140231+Ahmedhm1@users.noreply.github.com>
2025-09-19 14:40:31 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bst",
+    version="0.1.0",
+    description="A simple Binary Search Tree (BST) data structure in Python",
+    author="Ahmedhm1",
+    license="MIT",
+    packages=find_packages(),
+)
